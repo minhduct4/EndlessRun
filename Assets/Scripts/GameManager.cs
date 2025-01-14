0bf3fc4364017802950212f42c66b2658f7867ef
minhduct4 <nguyenminhduc@dtu.edu.vn>
2025-01-14 22:49:37 +0700

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject basicRoad; 
+    private Vector3 startPosition;
+    private Vector3 nextPosition;
+    public int numOfBasicRoad = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = new Vector3(0, 0, 0);
+        nextPosition = startPosition;
+        for (int i = 0; i < numOfBasicRoad; i++) // Tạo trước một số basicRoad
+        {
+            CreateBasicRoad();
+        } 
+    } 
+
+    public void CreateBasicRoad()
+    {  
+        var newBasicRoad = Instantiate(basicRoad, nextPosition, Quaternion.identity);
+        nextPosition = newBasicRoad.transform.Find("NextRoadPosition").transform.position; // cập nhật vị trí tạo basicRoad tiếp theo bằng cách lấy vị trí NextRoadPosition của basicRoad hiện tại
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float turnSpeed;
+    public float moveSpeed; 
+    private Rigidbody rb;
+    private float horizontalInput = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    } 
+    private void FixedUpdate()
+    {
+        // Giữ vận tốc cố định 
+        rb.linearVelocity = Vector3.forward * moveSpeed + new Vector3(horizontalInput * turnSpeed, rb.linearVelocity.y, 0);  
+    } 
+}
